ff6d045adc799d4cef781c072e9b1e7f00313468
Bruno Dias <brunoadias1996@gmail.com>
2022-06-24 20:44:53 -0300

    lidando com exceções

--- src/application/UI.java
+++ src/application/UI.java
@@ -26,6 +26,11 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -49,6 +54,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
+	
 	private static void printPiece(ChessPiece piece) {
     	if (piece == null) {
             System.out.print("-");
@@ -63,4 +69,7 @@ public class UI {
         }
         System.out.print(" ");
 	}
+
+
+
 }
