58cf25375bfffbcbc299775f4cca346127f60ceb
Bruno Dias <brunoadias1996@gmail.com>
2022-06-24 21:25:32 -0300

    movimentos da torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -39,6 +40,12 @@ public class ChessMatch {
 			throw new ChessException("sem movimentos possiveis para a peca");
 		}
 	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("movimento nao possivel");
+		}
+	} 
 	
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
