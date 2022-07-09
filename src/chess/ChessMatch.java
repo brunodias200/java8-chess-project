b7032230a72d6a29f0cb3ebc61156bfbe9d8960a
Bruno Dias <brunoadias1996@gmail.com>
2022-07-09 00:30:54 -0300

    promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -137,7 +136,7 @@ public class ChessMatch {
 			throw new IllegalStateException("there is no piece to be promoted");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Type does not exists");
+			return promoted;
 		}
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
